420782d4a2ec2f9a3c7b354633951eed0b2e1442
Rahul Roy <roy.rahulrr22@gmail.com>
2017-06-17 12:48:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+*
+* 
+* Version : 2.1.4
+* Style : FreeFlow.
+*
+*/
+window.onload=alert("Welcome, this is a loader or loading-indicator made with css!\nWorks best on mobile!\n\nWhat's new in v.2.1.4 :\n- Multi-step background animations\n- Bug fixes.\n\nTitle : Loading Indicator\nStyle : Fluid(FreeFlow)\nVersion : 2.x\n\nHave a great day ^^");
